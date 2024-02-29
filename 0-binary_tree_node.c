daf4439021a41c13b17c612633ca30afc9758c58
folujam <olusway@gmail.com>
2024-02-29 21:03:48 +0100

    .h and t0 - test

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function name(entry point)
+ * @parent: name of pointer to tree root
+ * @value: int
+ * Return: pointer to new node(binary_tree_t
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *nnode;
+
+	nnode = malloc(sizeof(binary_tree_t *));
+	if (nnode == NULL)
+		return (NULL);
+	nnode->n = value;
+	nnode->parent = parent;
+	nnode->left = NULL;
+	nnode->right = NULL;
+	return (nnode);
+}
