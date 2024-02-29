7b3fc6966893a66b9cbb93826c589350f5cdfef0
folujam <olusway@gmail.com>
2024-02-29 23:25:16 +0100

    mem well alloc

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *nnode;
 
-	nnode = malloc(sizeof(binary_tree_t *));
+	nnode = malloc(sizeof(binary_tree_t));
 	if (nnode == NULL)
 		return (NULL);
 	nnode->n = value;
